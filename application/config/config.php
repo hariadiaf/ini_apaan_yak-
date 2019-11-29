54b9b9d1ad13c10908270a4692800a4ad503c194
naufal mukhbit <nmukhbit@gmail.com>
2019-11-29 19:54:01 +0700

    Revert base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/impulse_imk/';
+$config['base_url'] = 'http://localhost/ini_apaan_yak/';
 
 /*
 |--------------------------------------------------------------------------
